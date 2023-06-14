64a465277d0480d595375f2e3704af89565257a2
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-14 10:01:10 +0530

    structure

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ function App() {
 }
 
 function CounterApp() {
+  // data member
+  let counter = 1;
+  let str = "Like Me!";
+  let id = 100;
+  let PI = 3.142;
+  let active = true;
+
+  //member function
+  let likeMeAction = () => {};
+  let dislikeMeAction = () => {};
+
+  // JSX::VIEW::USER-INTERFACE :: Required
   return (
     <>
-      <h1>1</h1>
-      <input type="button" value="Like Me!" />
+      <h1>{counter}</h1>
+      <input type="button" value={str} />
     </>
   );
 }
