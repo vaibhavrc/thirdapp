f457682fb182475f6b237c6d1bbeb716e9dd0166
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-14 09:49:20 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import AppHeader from "./pages/AppHeader";
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <AppHeader />;
-      <AppHeader />;
-      <AppBody />;
-      <AppFooter />;
+      <h1>Counter App</h1>
+      <CounterApp />
+    </>
+  );
+}
+
+function CounterApp() {
+  return (
+    <>
+      <h1>1</h1>
+      <input type="button" value="Like Me!" />
     </>
   );
 }
