ef5d6a2e8672f6a153b955d0d31926aa15791780
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-14 09:08:34 +0530

    multiple function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppHeader />;
+    <AppBody />;
+    </>
   );
 }
 
+function AppHeader(){
+  return (
+    <h1>AppHeader</h1>
+  );
+}
+function AppBody(){
+  return (
+    <h1>AppBody</h1>
+  );
+}
+function AppFooter(){
+  return (
+    <h1>AppFooter</h1>
+  );
+}
 export default App;
