f80f01c00a78b64dd1e9a0d5c2727f98319dca59
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-14 09:17:21 +0530

    "Header"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <AppHeader />;
-    <AppBody />;
+      <AppHeader />;
+      <AppBody />;
     </>
   );
 }
 
-function AppHeader(){
+function AppHeader() {
   return (
-    <h1>AppHeader</h1>
+    <div className="row bg-success text-light p-3">
+      <div className="col">Home</div>
+      <div className="col">Project</div>
+      <div className="col">About Us</div>
+    </div>
   );
 }
-function AppBody(){
+function AppBody() {
   return (
-    <h1>AppBody</h1>
+    <>
+      <div className="alert alert-success fs-3">lorem20 lorem20</div>
+    </>
   );
 }
-function AppFooter(){
-  return (
-    <h1>AppFooter</h1>
-  );
+function AppFooter() {
+  return <h1>AppFooter</h1>;
 }
 export default App;
